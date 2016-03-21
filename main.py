a5ab57e3f981fbd9ae0420b6318783fc4d6a5623
DaveHughesUWS <david.hughes@uws.ac.uk>
2016-03-21 20:28:45 +0000

    Updates from ShareLaTeX

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-cscan = np.genfromtxt("cscan.csv",delimiter = ",");
+cscan = np.genfromtxt("cscan-2.csv",delimiter = ",");
 
 plt.imshow(cscan);
 plt.show();
